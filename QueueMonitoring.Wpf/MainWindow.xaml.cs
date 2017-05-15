8a86f748a536f7f37f855986d28dc22124da6c5e
Harald Quist <harald.quist@kredinor.no>
2017-05-15 14:02:51 +0200

    Added wpf project

---
+++ QueueMonitoring.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace QueueMonitoring.Wpf
+{
+    using System;
+    using System.Reactive.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+    using Library;
+
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeTreeview();
+        }
+
+        private void InitializeTreeview()
+        {
+            var repository = new QueueRepository();
+            
+            var observable = repository.GetGrouping("coon_and_friends").Queues.ToObservable();
+
+            observable.Subscribe(ProcessMQueue);
+        }
+
+        private void ProcessMQueue(MQueue mQueue)
+        {
+            QueueTreeView.Items.Add(new TreeViewItem {Header = mQueue.Name});
+        }
+    }
+}
